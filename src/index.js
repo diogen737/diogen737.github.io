b5a383acaf3d2d9ecd5b15bb69d095151be52632
Oleg Musienko <Diogen737@gmail.com>
2020-04-15 22:41:36 +0300

    [master] Refactor styles structure; implement scroll-progress-bar

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-// Test import of a JavaScript function, an SVG, and Sass
-import { HelloWorld } from './js/HelloWorld'
-import WebpackLogo from './images/webpack-logo.svg'
 import './styles/index.scss'
 
-// Create SVG logo node
-const logo = document.createElement('img')
-logo.src = WebpackLogo
+const scrollBar = document.getElementById('scroll-bar');
 
-// Create heading node
-const greeting = document.createElement('h1')
-greeting.textContent = HelloWorld()
-
-// Append SVG and heading nodes to the DOM
-const app = document.querySelector('#root')
-app.append(logo, greeting)
+document.addEventListener('scroll', () => {
+    const winFullHeight = document.body.clientHeight;
+    const barWidth = 100.0 * window.scrollY / (winFullHeight - window.innerHeight);
+    scrollBar.style.width = `${barWidth}%`;
+});
