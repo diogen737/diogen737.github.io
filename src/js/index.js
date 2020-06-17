6901f755af8f249843d8a6c7c9e38e52580efb83
Oleg Musienko <Diogen737@gmail.com>
2020-06-17 17:59:47 +0300

    [layout] Layout footer; rearrange js files

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
-import './styles/index.scss';
+import '../styles/index.scss';
 
 const scrollBar = document.getElementById('scroll-bar');
-
 document.addEventListener('scroll', () => {
     const winFullHeight = document.body.clientHeight;
     const barWidth = 100.0 * window.scrollY / (winFullHeight - window.innerHeight);
     scrollBar.style.width = `${barWidth}%`;
 });
+
+const footerYear = document.getElementById('footer-year');
+footerYear.innerHTML = new Date().getFullYear();
