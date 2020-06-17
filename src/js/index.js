9ba8fbc40e0e82e6c497bcc7178553f52b966ae9
Oleg Musienko <Diogen737@gmail.com>
2020-06-17 19:57:43 +0300

    [layout] Add Github repository link

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,19 @@
 import '../styles/index.scss';
 
 const scrollBar = document.getElementById('scroll-bar');
+const githubLink = document.getElementById('github-link');
 document.addEventListener('scroll', () => {
+    /**
+     * Top scroll idicator
+     */
     const winFullHeight = document.body.clientHeight;
     const barWidth = 100.0 * window.scrollY / (winFullHeight - window.innerHeight);
     scrollBar.style.width = `${barWidth}%`;
+
+    /**
+     * Collapse GitHub button while scrolling
+     */
+    window.scrollY ? githubLink.classList.add('collapsed') : githubLink.classList.remove('collapsed');
 });
 
 const footerYear = document.getElementById('footer-year');
